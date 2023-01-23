51b2326e93dcb6eeb0995db00fb6269154e217d8
yoshi-cat <yoshitsugu.kosaka@more.global>
2023-01-23 15:42:23 +0000

    initial

--- src/components/Top/Nav.jsx
+++ src/components/Top/Nav.jsx
@@ -3,23 +3,23 @@ import React, { useRef, useState } from "react";
 import * as THREE from 'three'
 
 import { Scroll, ScrollControls, Environment } from '@react-three/drei'
-import { Turn as Hamburger } from 'hamburger-react'
+// import { Turn as Hamburger } from 'hamburger-react'
 
 
 const Nav = () => {
-   const [isOpen, setOpen] = useState(false)
+   // const [isOpen, setOpen] = useState(false)
 
-   return (
-      <Hamburger className='hamburger'
-         style={{
-            marginRight: '0%'
-         }}
-         size={40}
-         distance="sm"
-         toggled={isOpen} toggle={setOpen}
-      // onToggle={toggled => setOpen(!isOpen)}
-      />
-   )
+   // return (
+   //    <Hamburger className='hamburger'
+   //       style={{
+   //          marginRight: '0%'
+   //       }}
+   //       size={40}
+   //       distance="sm"
+   //       toggled={isOpen} toggle={setOpen}
+   //    // onToggle={toggled => setOpen(!isOpen)}
+   //    />
+   // )
 };
 
 const SubText = () => {
