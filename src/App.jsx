5477437f54db2cd8ee41c82798cac043a7e0d687
yoshi-cat <yoshitsugu.kosaka@more.global>
2023-02-07 21:25:45 +0000

    title fixed

--- src/App.jsx
+++ src/App.jsx
@@ -222,11 +222,10 @@ function App() {
         <Environment preset="warehouse" />
         <Rig /> 
         <Model modelPath={"https://thinkuldeep.com/modelviewer/Astronaut.glb"} />
-
       </Suspense>
       <OrbitControls makeDefault />
           <ScrollControls
-            pages={1.1} // Each page takes 100% of the height of the canvas
+            pages={2.2} // Each page takes 100% of the height of the canvas
             distance={1} // A factor that increases scroll bar travel (default: 1)
             damping={4} // Friction, higher is faster (default: 4)
             horizontal={false} // Can also scroll horizontally (default: false)
