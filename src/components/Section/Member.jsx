7f6c8cd0b7fa02552d2ae345d0ca750ff3b06c40
Dwayne Paisley-Marshall <superdwayne@gmail.com>
2023-07-31 21:47:07 +0200

    Added ew

--- src/components/Section/Member.jsx
+++ src/components/Section/Member.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-function Members(props) {
 
-  const twitterHandle = props.twitter;
+function Members(props) {
+  const { twitter, instagram, linkedin } = props;
 
   // Construct the full Twitter URL
-  const twitterUrl = `https://twitter.com/${twitterHandle}`;
+  const twitterHandle = twitter ? `https://twitter.com/${twitter}` : null;
+
   return (
     /* Write logic to select a random creative */
     <>
       <section className="Creative">
         <section className="Creative_name_wrapper">
-          
           <div className="Creative_name_bk">
             <h3>{props.bkname}</h3>
             <h3>{props.bkname}</h3>
@@ -30,27 +30,31 @@ function Members(props) {
         <div className="Creative_profile">
           <img src={props.imageSrc} alt={props.imageSrcAlt} />
           <h1 className="Creative_name">{props.nameMain}</h1>
-          
           <small>{props.company}</small>
           <p>{props.introDescription}</p>
           <section className="social">
-            <section>
-              <a href={props.instagram} target="_blank">
-              <img src="/images/insta.png"  alt="insta" />
-              </a>
-              </section>
+            {instagram && (
               <section>
-                <a href={props.linkedin} target="_blank">
-                  <img src="/images/in.png"  alt="Linked-in" />
+                <a href={instagram} target="_blank">
+                  <img src="/images/insta.png" alt="insta" />
                 </a>
               </section>
+            )}
+            {linkedin && (
               <section>
-                <a href={twitterUrl} target="_blank">
-                  <img src="/images/tw.png"  alt="Twitter" />
+                <a href={linkedin} target="_blank">
+                  <img src="/images/in.png" alt="Linked-in" />
                 </a>
               </section>
+            )}
+            {twitter && (
+              <section>
+                <a href={twitterHandle} target="_blank">
+                  <img src="/images/tw.png" alt="Twitter" />
+                </a>
+              </section>
+            )}
           </section>
-          
         </div>
       </section>
     </>
